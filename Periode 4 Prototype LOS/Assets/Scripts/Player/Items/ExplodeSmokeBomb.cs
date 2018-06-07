e04ed688e0af91eeab5344b1308c488275907b04
LSThimo <thimoyo@gmail.com>
2018-06-07 13:59:03 +0200

    Added smoke bomb
    TODO: Add particles

---
+++ Periode 4 Prototype LOS/Assets/Scripts/Player/Items/ExplodeSmokeBomb.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeSmokeBomb : MonoBehaviour
+{
+    public float timeToExplode;
+    private float countdownToExplode;
+
+    public float timeToDestroySmoke;
+    private float countdownToDestroySmoke;
+
+    public float timeToDestroyBomb;
+    private float countdownToDestroyBomb;
+
+    public float smokeRadius;
+
+    private GameObject smokeSphere;
+    public GameObject smokeParticles;
+
+    private bool hasExploded;
+
+    void Start()
+    {
+        smokeSphere = transform.GetChild(0).gameObject;
+        smokeSphere.transform.localScale *= smokeRadius;
+
+        countdownToExplode = timeToExplode;
+        countdownToDestroySmoke = timeToDestroySmoke;
+        countdownToDestroyBomb = timeToDestroyBomb;
+    }
+
+    void Update()
+    {
+        countdownToExplode -= Time.deltaTime;
+
+        if (countdownToExplode <= 0 && hasExploded == false)
+        {
+            Explode();
+        }
+
+        countdownToDestroyBomb -= Time.deltaTime;
+
+        if (countdownToDestroyBomb <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if (hasExploded == true)
+        {
+            countdownToDestroySmoke -= Time.deltaTime;
+
+            if (countdownToDestroySmoke <= 0)
+            {
+                smokeSphere.SetActive(false);
+            }
+        }
+    }
+
+    void Explode()
+    {
+        smokeSphere.SetActive(true);
+        hasExploded = true;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        Rigidbody smokeRigidbody = transform.GetComponent<Rigidbody>();
+        smokeRigidbody.constraints = RigidbodyConstraints.FreezePositionX;
+        smokeRigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+
+        //GameObject myParticles = Instantiate(smokeParticles, smokeSphere.transform.position, Quaternion.identity);
+    }
+}
---
+++ Periode 4 Prototype LOS/Assets/Scripts/Player/Items/SmokeBombThrower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeBombThrower : MonoBehaviour
+{
+    public GameObject smokeGrenade;
+    public float throwingForce;
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Throw();
+        }
+
+    }
+
+    void Throw()
+    {
+        //Instantiate object
+        GameObject mySmoke = Instantiate(smokeGrenade, transform.position, Quaternion.identity);
+
+        //Add force
+        Rigidbody smokeRb = mySmoke.GetComponent<Rigidbody>();
+        smokeRb.AddForce(transform.forward * throwingForce, ForceMode.VelocityChange);
+    }
+}
